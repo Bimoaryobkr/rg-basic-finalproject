e7748a1a38944fae5c40bcdd0897409095a70c7b
Bimo Aryo Bramantyo KR <101321869+Bimoaryobkr@users.noreply.github.com>
2023-06-14 22:05:56 +0700

    Add files via upload

---
+++ deployData.js
@@ -0,0 +1,15 @@
+/*
+  buatlah beberapa variable untuk pengecekan deployment
+
+  studentName: berisi string nama student
+  studentId: berisi string CAMPID
+  NetlifyDeployUrl: berisi string url Netlify deployment
+*/
+
+const studentName = 'BIMO ARYO BRAMANTYO KR';
+
+const studentId = 'FE4232403';
+
+const NetlifyDeployUrl = 'https://rg-final-project-basic-fe.netlify.app/client/';
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
